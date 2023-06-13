22ecd131aa941e976e8aa1b2b2860c8282f812ca
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-06-13 13:44:30 -0300

    first commit

---
+++ src/main/java/com/sistemasactivos/ms/accessmgt/AccessmgtApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.ms.accessmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessmgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemasactivos/ms/accessmgt/AccessmgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.ms.accessmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessmgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
