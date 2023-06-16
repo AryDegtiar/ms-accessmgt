38d4d232f1858038919209be97d11d2922b88778
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-06-16 15:15:07 -0300

    token funcionadno correctamente

--- src/main/java/com/sistemasactivos/ms/accessmgt/config/TokenUtils.java
+++ src/main/java/com/sistemasactivos/ms/accessmgt/config/TokenUtils.java
@@ -1,26 +1,26 @@
 package com.sistemasactivos.ms.accessmgt.config;
 
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+/**
+ *
+ * @author PW17634
+ */
 public class TokenUtils {
     public final static String ACCESS_TOKEN_SECRET = "4qhq8LrEBfYcaRHxhdb9zURb2rf8e7Ud";
-    private final static Long ACCESS_TOKEN_LIFE = 900L; // 15 minutes
+    private final static Long ACCESS_TOKEN_LIFE = 900L; // 15 minutos
 
-    public static String createToken (String name, String email){
-        long expirationTime = ACCESS_TOKEN_LIFE * 1000; // Convert seconds to milliseconds
+    public static String createToken(String name, String email){
+        long expirationTime =  ACCESS_TOKEN_LIFE * 1000;
         Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
-
-        System.out.println("Expiration date: " + expirationDate);
 
         Map<String, Object> extra = new HashMap<>();
         extra.put("name", name);
@@ -28,25 +28,27 @@ public class TokenUtils {
         return Jwts.builder()
                 .setSubject(email)
                 .setExpiration(expirationDate)
-                .setClaims(extra)
+                .addClaims(extra)
                 .signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes()))
                 .compact();
     }
 
-    public static UsernamePasswordAuthenticationToken getAuthentication(String token) {
+    public static UsernamePasswordAuthenticationToken getAuthentication(String token){
         String email;
 
         try {
             Claims claims = Jwts.parserBuilder()
-                    .setSigningKey(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes()))
+                    .setSigningKey(ACCESS_TOKEN_SECRET.getBytes())
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
+
             email = claims.getSubject();
+
             return new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
-        }catch (JwtException e){
+
+        }catch(JwtException e){
             return null;
         }
     }
-
 }
